2ba23adb668c97ab4d0c12a15b0da31d5643a57d
SNEEZxd <88846720+SNEEZxd@users.noreply.github.com>
2023-03-06 17:50:09 +0100

    home, reviews

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect,request
 
 app = Flask(__name__)
 
-class API:
-    
-    @app.route('/')
-    def home(self):
-        return render_template('home.html')
-    
-    @app.route('/about')
-    def about(self):
-        return render_template('about.html')
-    
-    @app.route('/products')
-    def products(self):
-        return render_template('products.html')
-    
-    @app.route('/reviews')
-    def reviews(self):
-        return render_template('reviews.html')
-    
-api = API()
+@app.route('/')
+def home():
+    return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/review', methods=['GET'])
+def reviews():
+    return render_template('reviews.html')
+
+    # @app.route('api/review', methods=['POST'])     
+    # def review(self):
+        
 if __name__ == '__main__':
     app.run(debug=True)
