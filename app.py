b48289d9d2341930f9b5c51d89c5ef34f44687eb
SNEEZxd <88846720+SNEEZxd@users.noreply.github.com>
2023-03-10 20:48:43 +0100

    update

--- app.py
+++ app.py
@@ -19,16 +19,19 @@ def product():
     
     if request.method == 'POST':
         product_id = request.form["product_id"]
+        
         if product_id == "":
             return render_template('getProduct.html',error = "Product does not exist")
-        
-        if ceneo_craper.check_product_id(product_id):
-            
-            ceneo_craper.get_product_data()
-            product_data = ceneo_craper.get_reviews()
+        product_data = ceneo_craper.check_database(product_id)
+        if product_data != {}:
             return render_template('product.html',product_data = product_data)
         else:
-            return render_template('getProduct.html',error = "Product does not exist")
+            if ceneo_craper.check_product_id(product_id):
+                ceneo_craper.get_product_data()
+                product_data = ceneo_craper.get_reviews()
+                return render_template('product.html',product_data = product_data)
+            else:
+                return render_template('getProduct.html',error = "Product does not exist")
         
 @app.route('/products', methods=['GET','POST'])
 def products():
